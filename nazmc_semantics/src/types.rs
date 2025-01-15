4962a103082b34ec30158518c1ee0adc7417cbf6
sherif-ibn-nasser <sherif2102003@gmail.com>
2025-01-15 11:20:22 +0200

    Analyze ptr and ref types

--- nazmc_semantics/src/types.rs
+++ nazmc_semantics/src/types.rs
@@ -1,20 +1,68 @@
-use std::path;
+use std::{net::UdpSocket, path};
 
 use typed_ast::{FieldsStruct, TupleStruct, Type, TypeKey};
 
 use crate::*;
 
 impl<'a> SemanticsAnalyzer<'a> {
+    const PTR_SIZE: i32 = usize::BITS as i32 / 8;
     pub(crate) fn analyze_type_expr(&mut self, type_expr_key: TypeExprKey) -> (TypeKey, i32, u8) {
         let type_expr = &self.ast.types_exprs.all[type_expr_key];
         let (typ, size, align) = match type_expr {
             TypeExpr::Path(path_type_expr_key) => self.analyze_path_type_expr(*path_type_expr_key),
-            TypeExpr::Paren(paren_type_expr_key) => todo!(),
-            TypeExpr::Slice(slice_type_expr_key) => todo!(),
-            TypeExpr::Ptr(ptr_type_expr_key) => todo!(),
-            TypeExpr::Ref(ref_type_expr_key) => todo!(),
-            TypeExpr::PtrMut(ptr_mut_type_expr_key) => todo!(),
-            TypeExpr::RefMut(ref_mut_type_expr_key) => todo!(),
+            TypeExpr::Paren(paren_type_expr_key) => {
+                return self.analyze_type_expr(
+                    self.ast.types_exprs.parens[*paren_type_expr_key].underlying_typ,
+                );
+            }
+            TypeExpr::Slice(slice_type_expr_key) => {
+                let underlying_typ =
+                    self.ast.types_exprs.slices[*slice_type_expr_key].underlying_typ;
+                let (type_key, _size, _align) = self.analyze_type_expr(underlying_typ);
+                (Type::Slice(type_key), -1, 0)
+            }
+            TypeExpr::Ptr(ptr_type_expr_key) => {
+                let underlying_typ = self.ast.types_exprs.ptrs[*ptr_type_expr_key].underlying_typ;
+                let (underlying_type_key, _size, _align) = self.analyze_type_expr(underlying_typ);
+                (
+                    Type::Ptr(underlying_type_key),
+                    Self::PTR_SIZE,
+                    Self::PTR_SIZE as u8,
+                )
+            }
+            TypeExpr::Ref(ref_type_expr_key) => {
+                let underlying_typ = self.ast.types_exprs.refs[*ref_type_expr_key].underlying_typ;
+                let (underlying_type_key, _size, _align) = self.analyze_type_expr(underlying_typ);
+                let underlying_typ_expr = self.ast.types_exprs.all[underlying_typ];
+                let (size, align) = if let TypeExpr::Slice(_) = underlying_typ_expr {
+                    (2 * Self::PTR_SIZE, 2 * Self::PTR_SIZE as u8)
+                } else {
+                    (Self::PTR_SIZE, Self::PTR_SIZE as u8)
+                };
+                (Type::Ref(underlying_type_key), size, align)
+            }
+            TypeExpr::PtrMut(ptr_mut_type_expr_key) => {
+                let underlying_typ =
+                    self.ast.types_exprs.ptrs_mut[*ptr_mut_type_expr_key].underlying_typ;
+                let (underlying_type_key, _size, _align) = self.analyze_type_expr(underlying_typ);
+                (
+                    Type::Ptr(underlying_type_key),
+                    Self::PTR_SIZE,
+                    Self::PTR_SIZE as u8,
+                )
+            }
+            TypeExpr::RefMut(ref_mut_type_expr_key) => {
+                let underlying_typ =
+                    self.ast.types_exprs.refs_mut[*ref_mut_type_expr_key].underlying_typ;
+                let (underlying_type_key, _size, _align) = self.analyze_type_expr(underlying_typ);
+                let underlying_typ_expr = self.ast.types_exprs.all[underlying_typ];
+                let (size, align) = if let TypeExpr::Slice(_) = underlying_typ_expr {
+                    (2 * Self::PTR_SIZE, 2 * Self::PTR_SIZE as u8)
+                } else {
+                    (Self::PTR_SIZE, Self::PTR_SIZE as u8)
+                };
+                (Type::RefMut(underlying_type_key), size, align)
+            }
             TypeExpr::Tuple(tuple_type_expr_key) => todo!(),
             TypeExpr::Array(array_type_expr_key) => todo!(),
             TypeExpr::Lambda(lambda_type_expr_key) => todo!(),
@@ -51,12 +99,12 @@ impl<'a> SemanticsAnalyzer<'a> {
         }
 
         match info.id_key {
-            IdKey::I_TYPE => (Type::I, isize::BITS as i32 / 8, isize::BITS as u8 / 8),
+            IdKey::I_TYPE => (Type::I, Self::PTR_SIZE, Self::PTR_SIZE as u8),
             IdKey::I1_TYPE => (Type::I1, 1, 1),
             IdKey::I2_TYPE => (Type::I2, 2, 2),
             IdKey::I4_TYPE => (Type::I4, 4, 4),
             IdKey::I8_TYPE => (Type::I8, 8, 8),
-            IdKey::U_TYPE => (Type::U, usize::BITS as i32 / 8, usize::BITS as u8 / 8),
+            IdKey::U_TYPE => (Type::U, Self::PTR_SIZE, Self::PTR_SIZE as u8),
             IdKey::U1_TYPE => (Type::U1, 1, 1),
             IdKey::U2_TYPE => (Type::U2, 2, 2),
             IdKey::U4_TYPE => (Type::U4, 4, 4),
